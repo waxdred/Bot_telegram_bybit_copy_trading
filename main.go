d294ccc17494e215647e64c44fc84b7487e5b413
waxcoin <waxcoin@hotmail.com>
2022-09-02 09:22:28 +0200

    feat: Go testnet 1week for see if everything is done

--- main.go
+++ main.go
@@ -8,19 +8,19 @@ import (
 	"bybit/bybit/telegram"
 	"bybit/env"
 	"log"
+	"os/exec"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func run(updates tgbotapi.UpdatesChannel, order *bybit.Bot, trade *bybit.Trades, api env.Env) {
 	for update := range updates {
-		log.Println(update.ChannelPost.From)
 		if update.ChannelPost != nil {
 			msg := update.ChannelPost.Text
 			dataBybite, err := telegram.ParseMsg(msg, order.Debeug)
 			if err == nil && dataBybite.Trade {
 				price := get.GetPrice(dataBybite.Currency, api)
-				if price.RetCode == 0 {
+				if price.RetCode == 0 && price.Result[0].BidPrice != "" {
 					if trade.Add(api, dataBybite, price) {
 						post.PostIsoled(api, dataBybite.Currency, trade, order.Debeug)
 						err = post.PostOrder(dataBybite.Currency, api, trade, order.Debeug)
@@ -38,6 +38,8 @@ func run(updates tgbotapi.UpdatesChannel, order *bybit.Bot, trade *bybit.Trades,
 					if order.Debeug {
 						trade.Print()
 					}
+				} else {
+					log.Printf("Symbol not found")
 				}
 			} else if err == nil && dataBybite.Cancel {
 				cancelErr := post.CancelOrder(dataBybite.Currency, api, trade)
@@ -54,13 +56,18 @@ func run(updates tgbotapi.UpdatesChannel, order *bybit.Bot, trade *bybit.Trades,
 	}
 }
 
+func runPython() {
+	out := exec.Command("python3 ./python/telegram.py")
+	log.Println(out.Output)
+}
+
 func main() {
 	var api env.Env
 	var order bybit.Bot
 	var trade bybit.Trades
 
 	// for show debeug set at true
-	order.NewBot(&trade, true)
+	order.NewBot(&trade, false)
 	err := env.LoadEnv(&api)
 	if err != nil {
 		log.Fatalf("Error cannot Read file .env")
@@ -71,7 +78,7 @@ func main() {
 		log.Panic(err)
 	}
 
-	botapi.Debug = true
+	botapi.Debug = false
 
 	log.Printf("Authorized on account %s", botapi.Self.UserName)
 
@@ -79,6 +86,7 @@ func main() {
 	u.Timeout = 60
 
 	updates := botapi.GetUpdatesChan(u)
+	// go runPython()
 	go listen.GetPositionOrder(api, &trade, &order)
 	run(updates, &order, &trade, api)
 }
--- python/telegram.py
+++ python/telegram.py
@@ -4,42 +4,52 @@ from pathlib import Path
 
 import os
 
+from telethon.tl.custom import message
 
 dotenv_path = Path('./.env')
 load_dotenv(dotenv_path=dotenv_path)
-if os.path.exists("_log"):
-    os.remove("_log")
 
-session = "trading bot"
-api_id = os.getenv('API_ID')
-api_hash = os.getenv('API_HASH')
-id_channel = os.getenv('ID_CHANNEL')
-proxy = None
+class Telegram():
+    def __init__(self):
+        self.HOST = '127.0.0.1'
+        self.PORT = 30000
+        self.api_id = os.getenv('API_ID')
+        self.api_hash = os.getenv('API_HASH')
+        self.id_channel = os.getenv('ID_CHANNEL')
+        self.my_channel = os.getenv('MY_CHANNEL')
+        self.session = "trading bot"
+        self.proxy = None
+        self.msg = ""
+        self.client = TelegramClient(self.session, self.api_id, self.api_hash, proxy=self.proxy)
 
-async def handler(update):
-    t = str(update)
-    pos = t.find(str(id_channel))
-    t = t[pos:]
-    pos = t.find("message=")
-    if pos != -1:
-        t = t[pos + len("message= "):]
-        pos = t.find("'")
+    async def handler(self, update):
+        t = str(update)
+        pos = t.find(str(self.id_channel))
+        t = t[pos:]
+        pos = t.find("message=")
         if pos != -1:
-            if os.path.exists("_log"):
-                os.remove("_log")
-            t = t[:pos]
+            t = t[pos + len("message= "):]
+            pos = t.find("'")
+            if pos != -1:
+                if os.path.exists("_log"):
+                    os.remove("_log")
+                t = t[:pos]
+                t = t.replace("\\n", "\n", -1)
+                print(t)
+                await self.client.send_message(entity=int(self.my_channel), message=t)
+        return t
+
+    def start(self):
+        with self.client:
+            # Register the update handler so that it gets called
+            t = self.client.add_event_handler(self.handler)
             print(t)
-            f = open("_log", "w")
-            f.write(t)
-            f.close()
+        
+            # Run the client until Ctrl+C is pressed, or the client disconnects
+            self.client.run_until_disconnected()
 
-
-# Use the client in a `with` block. It calls `start/disconnect` automatically.
-with TelegramClient(session, api_id, api_hash, proxy=proxy) as client:
-    # Register the update handler so that it gets called
-    client.add_event_handler(handler)
-
-    # Run the client until Ctrl+C is pressed, or the client disconnects
-    client.run_until_disconnected()
-
-f.close()
+if __name__ == '__main__':
+    print("Run Python")
+    telegram = Telegram()
+    print("Class init....")
+    telegram.start()
